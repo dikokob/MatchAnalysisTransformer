58fdde6e5dac9501f5df0f175c30ef28465af9b9
BoitumeloDikoko <dikokob@gmail.com>
2020-08-21 14:19:35 +0200

    columns added to data structure

--- SQLscripts/AerialDuelsSetPieces/AerialDuelsSetPieces.cs
+++ SQLscripts/AerialDuelsSetPieces/AerialDuelsSetPieces.cs
@@ -8,7 +8,17 @@ namespace MatchAnalysisWriteFilesToSqlServerFunction
     {
         public static readonly List<DataTypeColumnMap> ColumnMaps = new List<DataTypeColumnMap>{
             new DataTypeColumnMap("Set Piece OPTA Event ID", typeof(int)),
-
+            new DataTypeColumnMap("Aerial Duel OPTA ID", typeof(int)),
+            new DataTypeColumnMap("Aerial Duel OPTA ID", typeof(string)),
+            new DataTypeColumnMap("Aerial Duel Player Name", typeof(string)),
+            new DataTypeColumnMap("Aerial Duel Team ID", typeof(string)),
+            new DataTypeColumnMap("Aerial Duel Team Name", typeof(string)),
+            new DataTypeColumnMap("Successfull/Unsuccessful", typeof(string)),
+            new DataTypeColumnMap("Other Aerial Duel Player ID", typeof(string)),
+            new DataTypeColumnMap("Other Aerial Duel Player Name", typeof(string)),
+            new DataTypeColumnMap("Other Aerial Duel Team ID", typeof(string)),
+            new DataTypeColumnMap("Other Aerial Duel Team Name", typeof(string)),
+            new DataTypeColumnMap("Aerial Duel Is Shot", typeof(boolean))
             };
 
 

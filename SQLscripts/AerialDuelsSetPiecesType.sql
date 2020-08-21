80a0435470c259386899734ce6a59f5d63b908eb
BoitumeloDikoko <dikokob@gmail.com>
2020-08-21 11:18:13 +0200

    inital commit

---
+++ SQLscripts/AerialDuelsSetPiecesType.sql
@@ -0,0 +1,7 @@
+CREATE TYPE [dbo].[AerialDuelsSetPiecesType] AS TABLE(
+	[Set Piece OPTA Event ID] [int] NULL,
+
+)
+GO
+
+
---
+++ SQLscripts/CreateAerialSetPiecesStoredProcedure.sql
@@ -0,0 +1,42 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[spRemoveExistingAerialDuelsSetPieces] @AerialDuelsSetPiecesData [dbo].[AerialDuelsSetPiecesType] READONLY
+AS
+BEGIN
+	DECLARE @GameID as nvarchar(500);
+	DECLARE @CompetitionID as NVARCHAR(100);
+
+	DECLARE @Cursor as CURSOR;
+
+	SET @Cursor = CURSOR FOR
+	SELECT DISTINCT [Game ID], [Competition ID] FROM @AerialDuelsSetPiecesData;
+
+	OPEN @Cursor;
+	FETCH NEXT FROM @Cursor INTO @GameID, @CompetitionID;
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+	 PRINT 'Deleting AerialDuelsSetPieces for GameID: ' + @GameID + ', CompetitonID: ' + @CompetitionID;
+	 DELETE FROM [dbo].[AerialDuelsSetPieces] where Id IN (SELECT Id FROM [dbo].[AerialDuelsSetPieces] where [Game ID] = @GameID AND [Competition ID] = @CompetitionID)
+	 FETCH NEXT FROM @Cursor INTO @GameID, @CompetitionID;
+	END
+
+	CLOSE @Cursor;
+	DEALLOCATE @Cursor;
+
+    INSERT [dbo].[AerialDuelsSetPieces](
+	[Set Piece OPTA Event ID],
+
+	)
+    SELECT * FROM @AerialDuelsSetPiecesData
+END
+GO
+
+

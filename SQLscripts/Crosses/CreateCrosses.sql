7a45f37c9edf380a7e539ab7c1b7090955db2655
BoitumeloDikoko <dikokob@gmail.com>
2020-08-24 16:43:47 +0200

    update sqlcrosses

--- SQLscripts/Crosses/CreateCrosses.sql
+++ SQLscripts/Crosses/CreateCrosses.sql
@@ -4,11 +4,58 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[AerialDuelsSetPieces](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[Crosses](
+    [ID] [int] IDENTITY(1,1) NOT NULL,
     [game_id] [nvarchar](50) NULL,
+    [Fixture] [nvarchar](50) NULL,
+    [Attackng Team] [nvarchar](50) NULL,
+    [Defending Team] [nvarchar](50) NULL,
+    [Attackng Team ID] [nvarchar](50) NULL,
+    [Defending Team ID] [nvarchar](50) NULL,
+    [Goals Scored] [int] NULL,
+    [Goals Conceled] [int] NULL,
+    [Goals Difference] [int] NULL,
+    [Game State] [nvarchar](50) NULL,
+    [Side] [nvarchar](50) NULL,
+    [Early/ Lateral Deep] [nvarchar](50) NULL,
+    [OPTA Event ID] [int] NULL,
+    [perid_id] [int] NULL,
+    [min] [int] NULL,
+    [sec] [int] NULL,
+    [X Coordinate] [float] NULL,
+    [Y Coordinate] [float] NULL,
+    [End X Coordinate] [float] NULL,
+    [End Y Coordinate] [float] NULL,
+    [Length Pass] [float] NULL,
+    [% Distance Along X] [float] NULL,
+    [Player ID] [nvarchar](50) NULL,
+    [Player Name] [nvarchar](50) NULL,
+    [Preferred Foot] [nvarchar](50) NULL,
+    [Outcome] [nvarchar](50) NULL,
+    [Keypass/Assist] [nvarchar](50) NULL,
+    [Blocked Pass] [bit] NULL,
+    [Cutback] [bit] NULL,
+    [OPTA Pull Back Qualifier] [bit] NULL,
+    [Out Of Pitch] [bit] NULL,
+    [Ending Too Wide] [bit] NULL,
+    [Cross Type] [nvarchar](50) NULL,
+    [Set Piece OPTA Event ID] [int] NULL,
+    [OPTA Cross Qualifier] [bit] NULL,
+    [Time Between Set Piece and Cross] [float] NULL,
+    [Number Events Between Set Piece and Cross] [int] NULL,
+    [Linked 2nd Phase Cross IDs] [nvarchar](50) NULL,
+    [First Contact Type] [int] NULL,
+    [First Contact Explanation] [nvarchar](50) NULL,
+    [First Contact Player ID] [nvarchar](50) NULL,
+    [First Contact Player Name] [nvarchar](50) NULL,
+    [First Contact Team ID] [nvarchar](50) NULL,
+    [First Contact Team Name] [nvarchar](50) NULL,
+    [First Contact Aerial] [bit] NULL,
+    [Defending Goalkeeper ID] [nvarchar](50) NULL,
+    [Defending Goalkeeper Name] [nvarchar](50) NULL
 
- CONSTRAINT [PK_AerialDuelsSetPieces] PRIMARY KEY CLUSTERED
+
+ CONSTRAINT [PK_Crosses] PRIMARY KEY CLUSTERED
 (
 	[ID] ASC
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
--- SQLscripts/Crosses/CreateCrossesStoredProcedure.sql
+++ SQLscripts/Crosses/CreateCrossesStoredProcedure.sql
@@ -6,7 +6,6 @@ GO
 
 
 
-
 CREATE PROCEDURE [dbo].[spRemoveExistingCrosses] @CrossesData [dbo].[CrossesType] READONLY
 AS
 BEGIN
@@ -33,9 +32,52 @@ BEGIN
 
     INSERT [dbo].[Crosses](
 	[game_id],
-
-
-
+	[Fixture],
+	[Attacking Team],
+	[Defending Team],
+	[Attacking Team ID],
+	[Defending Team ID],
+	[Goals Scored],
+	[Goals Conceded],
+	[Goals Difference],
+	[Game State],
+	[Side],
+	[Early/Lateral/Deep],
+	[OPTA Event ID],
+	[period_id],
+	[min],
+	[sec],
+	[X Coordinate],
+	[Y Coordinate],
+	[End X Coordinate],
+	[End Y Coordinate],
+	[Length Pass],
+	[% Distance Along X],
+	[Player ID],
+	[Player Name],
+	[Preferred Foot],
+	[Outcome],
+	[Keypass/Assist],
+	[Blocked Pass],
+	[Cutback],
+	[OPTA Pull Back Qulifier],
+	[Out Of Pitch],
+	[Ending Too Wide],
+	[Cross Type],
+	[Set Piece OPTA Event ID],
+	[OPTA Cross Qulifier],
+	[Time Between Set Piece and Cross],
+	[Number Events Between Set Piece and Cross],
+	[Linked 2nd Phase Cross IDs],
+	[First Contact Type],
+	[First Contact Explanation],
+	[First Contact Player ID],
+	[First Contact Player Name],
+	[First Contact Team ID],
+	[First Contact Team Name],
+	[First Contact Aerial],
+	[Defending Goalkeeper ID],
+	[Defending Goalkeeper Name]
 	)
     SELECT * FROM @CrossesData
 END

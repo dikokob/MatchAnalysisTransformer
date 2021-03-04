f8b29c02eedbfe59627cce62ae716c96556e0f4c
BoitumeloDikoko <dikokob@gmail.com>
2021-03-04 08:17:02 +0200

    update to spectrum match analysis and tracking data transformer
    updates on  spectrum match analysis are from FED
    updates on  tracking data transformer are corrections

--- Transformers/SpectrumMatchAnalysisTransformer.py
+++ Transformers/SpectrumMatchAnalysisTransformer.py
@@ -221,7 +221,7 @@ class SpectrumMatchAnalysisTransformer:
             # we first need to flatten the array
             all_players_and_frames_flattened = [item for sublist in all_players_and_frames for item in sublist]
             track_players_df = pd.DataFrame(all_players_and_frames_flattened,
-                                            columns=['Frame_Count', 'Team_ID', 'System_Target_ID', 'Jersey_Number',
+                                            columns=['Frame_ID', 'Team_ID', 'System_Target_ID', 'Jersey_Number',
                                                      'x_Player', 'y_Player', 'Speed_Player'])
 
             # let's try to use the same approach to get ball tracking data in a nicer format
@@ -233,21 +233,25 @@ class SpectrumMatchAnalysisTransformer:
             track_ball_df = pd.DataFrame(all_balls_and_frames)
             if track_ball_df.shape[1] > 7:
                 track_ball_df = track_ball_df.iloc[:, :7].reset_index(drop=True)
-            track_ball_df.columns = ['X_coord_ball', 'Y_coord_ball', 'Z_coord_ball', 'Speed_ball', 'Team_Possession',
-                                     'Ball_Status', 'Ball_contact_dev_info']
+            track_ball_df.columns = ['x_Ball', 'y_Ball', 'z_Ball', 'Speed_Ball', 'Last_Touched', 'Is_Ball_Live',
+                                     'Ball_contact_dev_info']
 
-            for col in ['X_coord_ball', 'Y_coord_ball', 'Z_coord_ball', 'Speed_ball']:
-                track_ball_df[col] = track_ball_df[col].astype(float)
+            for col in ['x_Ball', 'y_Ball', 'z_Ball']:
+                track_ball_df[col] = 0.01 * track_ball_df[col].astype(float)
+            for col in ['x_Player', 'y_Player']:
+                track_players_df[col] = 0.01 * track_players_df[col].astype(float)
+
+            track_ball_df['Speed_Ball'] = 0.01 * track_ball_df['Speed_Ball'].astype(float)
 
             # we now add the frame_count column
-            track_ball_df['Frame_Count'] = [int(x[k].split(':')[0]) for k in range(len(all_balls_and_frames))]
-            track_ball_df['Is_Ball_Live'] = np.where(track_ball_df['Ball_Status'] == 'Alive', 'true', 'false')
-            track_ball_df['Last_Touched'] = track_ball_df['Team_Possession']
+            track_ball_df['Frame_ID'] = [int(x[k].split(':')[0]) for k in range(len(all_balls_and_frames))]
+            # track_ball_df['Is_Ball_Live'] = np.where(track_ball_df['Ball_Status']=='Alive', 'true', 'false')
+            # track_ball_df['Last_Touched'] = track_ball_df['Team_Possession']
             # track_ball_df['Is_Home_Away'] = np.where()
 
             # we now join the data
-            all_tracking_data = track_players_df.merge(track_ball_df, how='inner', left_on=['Frame_Count'],
-                                                       right_on=['Frame_Count'])
+            all_tracking_data = track_players_df.merge(track_ball_df, how='inner', left_on=['Frame_ID'],
+                                                       right_on=['Frame_ID'])
 
             # we need start and end frames for first and second half
 
@@ -262,12 +266,26 @@ class SpectrumMatchAnalysisTransformer:
                 frame_ends.append(int(tracab_metadata['TracabMetaData']['match']['period'][i]['@iEndFrame']))
 
             all_tracking_data_splits = []
+            max_frame_period = 0
             for i in range(len(frame_starts)):
-                all_tracking_data_splits.append(all_tracking_data[(all_tracking_data.Frame_Count >= frame_starts[i]) & (
-                            all_tracking_data.Frame_Count <= frame_ends[i]) & (~all_tracking_data.Team_ID.isin(
-                    [-1, 4]))].reset_index(drop=True))
+                data_split = all_tracking_data[
+                    (all_tracking_data.Frame_ID >= frame_starts[i]) & (all_tracking_data.Frame_ID <= frame_ends[i]) & (
+                        ~all_tracking_data.Team_ID.isin([-1, 4]))].sort_values(['Frame_ID']).reset_index(drop=True)
+                data_split['Period_ID'] = i + 1
+                reps_per_frame = data_split[['Frame_ID', 'Team_ID']].groupby(
+                    ['Frame_ID']).count().reset_index().sort_values(['Frame_ID']).reset_index(drop=True)
+                data_split['Time'] = np.round(
+                    np.repeat(np.array(range(len(data_split.Frame_ID.unique()))) / 25, reps_per_frame.Team_ID.tolist()),
+                    2)
+                data_split['Frame_ID'] = np.round(data_split['Time'] * 25) + max_frame_period
+                all_tracking_data_splits.append(data_split)
+                max_frame_period = data_split['Frame_ID'].max()
 
             df = pd.concat(all_tracking_data_splits)
+            df['Is_Ball_Live'] = np.where(df['Is_Ball_Live'] == 'Alive', 'true', 'false')
+            df['Last_Touched'] = np.where(df['Last_Touched'] == 'A', 'away', 'home')
+            df['Is_Home_Away'] = np.where(df['Team_ID'] == 0, 'away', 'home')
+            df['z_Player'] = 0
             # all_tracking_data_final.drop_duplicates(['Frame_Count']).Team_Possession.value_counts()
 
         else:  # json file
@@ -279,8 +297,6 @@ class SpectrumMatchAnalysisTransformer:
             track_file.close()
 
             result = [json.loads(jline) for jline in str(raw_data).split('\n')]
-
-
 
             ball_df = pd.DataFrame(result)[
                 ['ball', 'frameIdx', 'period', 'gameClock', 'wallClock', 'lastTouch', 'live']]
@@ -702,7 +718,6 @@ class SpectrumMatchAnalysisTransformer:
         match_info['home_formation'] = home_formation
         match_info['away_formation'] = away_formation
 
-
         return df_player_names_raw, players_df_lineup, match_info
 
     @staticmethod
@@ -718,89 +733,116 @@ class SpectrumMatchAnalysisTransformer:
 
         """
 
-        if 'System_Target_ID' not in list(track_players_df.columns): #means we have output from second spectrum file
-            #time played by players
-            time_played_by_players = pd.DataFrame(0.04*track_players_df.groupby(['Is_Home_Away', 'Player_ID']).Frame_ID.count()).reset_index()
-            #time in-out possession for players
-            time_in_out_possession = pd.DataFrame(0.04*track_players_df[track_players_df.Is_Ball_Live=='true'].groupby(['Is_Home_Away', 'Player_ID', 'Last_Touched']).Frame_ID.count()).reset_index()
-            time_played_by_players.rename(columns = {'Frame_ID': 'time played'}, inplace = True)
-            time_in_out_possession['time in/out possession for the team'] = np.where(time_in_out_possession.Last_Touched==time_in_out_possession.Is_Home_Away, 'time in possession for the team', 'time out possession for the team').tolist()
-            time_in_out_possession.rename(columns = {'Is_Home_Away': 'home/away',
-                                                     'Player_ID': 'player id', 'Frame_ID': 'time possession'}, inplace = True)
+        if 'System_Target_ID' not in list(track_players_df.columns):  # means we have output from second spectrum file
+            # time played by players
+            time_played_by_players = pd.DataFrame(
+                0.04 * track_players_df.groupby(['Is_Home_Away', 'Player_ID']).Frame_ID.count()).reset_index()
+            # time in-out possession for players
+            time_in_out_possession = pd.DataFrame(
+                0.04 * track_players_df[track_players_df.Is_Ball_Live == 'true'].groupby(
+                    ['Is_Home_Away', 'Player_ID', 'Last_Touched']).Frame_ID.count()).reset_index()
+            time_played_by_players.rename(columns={'Frame_ID': 'time played'}, inplace=True)
+            time_in_out_possession['time in/out possession for the team'] = np.where(
+                time_in_out_possession.Last_Touched == time_in_out_possession.Is_Home_Away,
+                'time in possession for the team', 'time out possession for the team').tolist()
+            time_in_out_possession.rename(columns={'Is_Home_Away': 'home/away',
+                                                   'Player_ID': 'player id', 'Frame_ID': 'time possession'},
+                                          inplace=True)
 
-            final_data = time_played_by_players.merge(time_in_out_possession, how = 'inner',
-                                                      left_on = ['Is_Home_Away', 'Player_ID'], right_on = ['home/away', 'player id'])[['Player_ID', 'time played', 'time possession', 'time in/out possession for the team']]
+            final_data = time_played_by_players.merge(time_in_out_possession, how='inner',
+                                                      left_on=['Is_Home_Away', 'Player_ID'],
+                                                      right_on=['home/away', 'player id'])[
+                ['Player_ID', 'time played', 'time possession', 'time in/out possession for the team']]
             columns_final_data = list(final_data)
             column_pivot = 'time in/out possession for the team'
             value_pivot = 'time possession'
 
-            #now, pivot the data based on the 'in-out' column
-            final_data_pivot = final_data.fillna(-999).pivot_table(index = [x for x in columns_final_data if x not in [column_pivot, value_pivot]],
-                                                                   columns = column_pivot, values = value_pivot, dropna = True).reset_index().replace(-999, np.nan)
+            # now, pivot the data based on the 'in-out' column
+            final_data_pivot = final_data.fillna(-999).pivot_table(
+                index=[x for x in columns_final_data if x not in [column_pivot, value_pivot]],
+                columns=column_pivot, values=value_pivot, dropna=True).reset_index().replace(-999, np.nan)
 
 
-        else: #we are with the output from tracab
-            track_players_df['Team_ID'] = np.where(track_players_df['Team_ID']==0, 'A', 'H')
-            #time played by players
-            time_played_by_players = pd.DataFrame(0.04*track_players_df.groupby(['Team_ID', 'Jersey_Number']).Frame_Count.count()).reset_index()
-            #time in-out possession for players
-            #if alive_only:
-            time_in_out_possession = pd.DataFrame(0.04*track_players_df[track_players_df.Ball_Status=='Alive'].groupby(['Team_ID', 'Jersey_Number', 'Team_Possession']).Frame_Count.count()).reset_index()
-            time_played_by_players.rename(columns = {'Frame_Count': 'time played'}, inplace = True)
+        else:  # we are with the output from tracab
+            track_players_df['Team_ID'] = np.where(track_players_df['Team_ID'] == 0, 'A', 'H')
+            # time played by players
+            time_played_by_players = pd.DataFrame(
+                0.04 * track_players_df.groupby(['Team_ID', 'Jersey_Number']).Frame_Count.count()).reset_index()
+            # time in-out possession for players
+            # if alive_only:
+            time_in_out_possession = pd.DataFrame(
+                0.04 * track_players_df[track_players_df.Ball_Status == 'Alive'].groupby(
+                    ['Team_ID', 'Jersey_Number', 'Team_Possession']).Frame_Count.count()).reset_index()
+            time_played_by_players.rename(columns={'Frame_Count': 'time played'}, inplace=True)
 
-            time_in_out_possession['time in/out possession for the team'] = np.where(time_in_out_possession.Team_Possession==time_in_out_possession.Team_ID, 'time in possession for the team', 'time out possession for the team').tolist()
-            time_in_out_possession.rename(columns = {'Team_ID': 'home/away',
-                                                     'Frame_Count': 'time possession'}, inplace = True)
+            time_in_out_possession['time in/out possession for the team'] = np.where(
+                time_in_out_possession.Team_Possession == time_in_out_possession.Team_ID,
+                'time in possession for the team', 'time out possession for the team').tolist()
+            time_in_out_possession.rename(columns={'Team_ID': 'home/away',
+                                                   'Frame_Count': 'time possession'}, inplace=True)
 
-            final_data = time_played_by_players.merge(time_in_out_possession, how = 'inner',
-                                                      left_on = ['Team_ID', 'Jersey_Number'], right_on = ['home/away', 'Jersey_Number'])[['Team_ID', 'Jersey_Number', 'time played', 'time possession', 'time in/out possession for the team']]
+            final_data = time_played_by_players.merge(time_in_out_possession, how='inner',
+                                                      left_on=['Team_ID', 'Jersey_Number'],
+                                                      right_on=['home/away', 'Jersey_Number'])[
+                ['Team_ID', 'Jersey_Number', 'time played', 'time possession', 'time in/out possession for the team']]
             columns_final_data = list(final_data)
             column_pivot = 'time in/out possession for the team'
             value_pivot = 'time possession'
 
-            #now, pivot the data based on the 'in-out' column
-            final_data_pivot = final_data.fillna(-999).pivot_table(index = [x for x in columns_final_data if x not in [column_pivot, value_pivot]],
-                                                                   columns = column_pivot, values = value_pivot, dropna = True).reset_index().replace(-999, 0)
+            # now, pivot the data based on the 'in-out' column
+            final_data_pivot = final_data.fillna(-999).pivot_table(
+                index=[x for x in columns_final_data if x not in [column_pivot, value_pivot]],
+                columns=column_pivot, values=value_pivot, dropna=True).reset_index().replace(-999, 0)
 
-            #file_results = file_results
+            # file_results = file_results
             with open(file_results, encoding='utf-8') as fd:
                 match_results = xmltodict.parse(fd.read())
 
             player_shirts = []
             for i in range(2):
-                #team_id = match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['@TeamRef']
-                #team_name = np.where(team_id == 't' + str(int(home_team_id)), home_team_name, away_team_name).tolist()
+                # team_id = match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['@TeamRef']
+                # team_name = np.where(team_id == 't' + str(int(home_team_id)), home_team_name, away_team_name).tolist()
                 if type(match_results['SoccerFeed']['SoccerDocument']) == list:
                     for k in range(len(match_results['SoccerFeed']['SoccerDocument'])):
-                        if match_results['SoccerFeed']['SoccerDocument'][k]['@uID'] == 'f' + str(track_players_df.game_id.iloc[0]):
+                        if match_results['SoccerFeed']['SoccerDocument'][k]['@uID'] == 'f' + str(
+                                track_players_df.game_id.iloc[0]):
                             break
 
-                    home_away = match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i]['@Side'][0].upper() # Todo fix k being refrenced outside the loop
-                    for j in range(len(match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i]['PlayerLineUp']['MatchPlayer'])):
-                        player_id = match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i]['PlayerLineUp']['MatchPlayer'][j]['@PlayerRef']
-                        shirt_number = int(match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i]['PlayerLineUp']['MatchPlayer'][j]['@ShirtNumber'])
+                    home_away = match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i]['@Side'][
+                        0].upper()
+                    for j in range(len(match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i][
+                                           'PlayerLineUp']['MatchPlayer'])):
+                        player_id = \
+                        match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i]['PlayerLineUp'][
+                            'MatchPlayer'][j]['@PlayerRef']
+                        shirt_number = int(match_results['SoccerFeed']['SoccerDocument'][k]['MatchData']['TeamData'][i][
+                                               'PlayerLineUp']['MatchPlayer'][j]['@ShirtNumber'])
                         player_shirts.append([player_id, shirt_number, home_away])
 
                 else:
-                    home_away = match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['@Side'][0].upper()
-                    for j in range(len(match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['PlayerLineUp']['MatchPlayer'])):
-                        player_id = match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['PlayerLineUp']['MatchPlayer'][j]['@PlayerRef']
-                        shirt_number = int(match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['PlayerLineUp']['MatchPlayer'][j]['@ShirtNumber'])
+                    home_away = match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['@Side'][
+                        0].upper()
+                    for j in range(len(
+                            match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['PlayerLineUp'][
+                                'MatchPlayer'])):
+                        player_id = \
+                        match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['PlayerLineUp'][
+                            'MatchPlayer'][j]['@PlayerRef']
+                        shirt_number = int(
+                            match_results['SoccerFeed']['SoccerDocument']['MatchData']['TeamData'][i]['PlayerLineUp'][
+                                'MatchPlayer'][j]['@ShirtNumber'])
                         player_shirts.append([player_id, shirt_number, home_away])
 
-            player_shirts = pd.DataFrame(player_shirts, columns = ['Player_ID', 'Jersey_Number', 'home_away'])
+            player_shirts = pd.DataFrame(player_shirts, columns=['Player_ID', 'Jersey_Number', 'home_away'])
 
-            final_data_pivot = final_data_pivot.merge(player_shirts, how = 'inner',
-                                                      left_on = ['Team_ID', 'Jersey_Number'],
-                                                      right_on = ['home_away', 'Jersey_Number']).drop(['Team_ID', 'home_away',
-                                                                                                       'Jersey_Number'], axis = 1)[['Player_ID', 'time played', 'time in possession for the team',
-                                                                                                                                    'time out possession for the team']].reset_index(drop=True)
+            final_data_pivot = final_data_pivot.merge(player_shirts, how='inner',
+                                                      left_on=['Team_ID', 'Jersey_Number'],
+                                                      right_on=['home_away', 'Jersey_Number']).drop(
+                ['Team_ID', 'home_away',
+                 'Jersey_Number'], axis=1)[['Player_ID', 'time played', 'time in possession for the team',
+                                            'time out possession for the team']].reset_index(drop=True)
             final_data_pivot['Player_ID'] = list(map(lambda x: int(x.replace('p', '')), final_data_pivot['Player_ID']))
 
-        final_data_pivot = final_data_pivot.fillna(0) #to fill any na in time in possession or out possession
+        final_data_pivot = final_data_pivot.fillna(0)  # to fill any na in time in possession or out possession
 
         return final_data_pivot
-
-
-
-

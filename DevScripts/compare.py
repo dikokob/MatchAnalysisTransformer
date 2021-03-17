6a4612d93c5891e8b9afd182bc71f4998452aaa4
JesmaiasDavid <jesmaiasdavid@gmail.com>
2021-03-17 14:57:01 +0200

    comments added

--- DevScripts/compare.py
+++ DevScripts/compare.py
@@ -1,16 +1,24 @@
 import pandas as pd
 
+# read file
 azureCrosses = pd.read_csv('AzureCrosses.csv')
+# replace with empty value, all rows in game_id that start by 'g'
 azureCrosses['game_id'].replace(regex=True, inplace=True, to_replace=r'g', value=r'')
+# replace with empty value, all rows in game_id that start by 'f'
 azureCrosses['game_id'].replace(regex=True, inplace=True, to_replace=r'f', value=r'')
+# list of unique game ids
 azureCrosses_game_id = list(azureCrosses['game_id'].unique())
 
 del azureCrosses
-
+# read file
 azureOptaCoreStats = pd.read_csv('AzureOptaCoreStats.csv')
+# replace with empty value, all rows in game_id that start by 'g'
 azureOptaCoreStats['Game ID'].replace(regex=True, inplace=True, to_replace=r'g', value=r'')
+# replace with empty value, all rows in game_id that start by 'f'
 azureOptaCoreStats['Game ID'].replace(regex=True, inplace=True, to_replace=r'f', value=r'')
+# list of unique game ids
 azureOptaCoreStats_game_id = list(azureOptaCoreStats['Game ID'].unique())
+# Group Opta Core Stats
 azureOptaCoreStatsGroupByCount = azureOptaCoreStats[['Competition Name', 'Game ID', 'Season Name']].drop_duplicates() \
     .groupby(['Competition Name', 'Season Name']).agg({"Game ID": "count"})
 
@@ -26,9 +34,13 @@ allOptaCoreStatsGroupByCount = allOptaCoreStats[['Competition Name', 'Game ID', 
 
 # del allOptaCoreStats
 
+#read file
 azureSecondPhaseSetPieces = pd.read_csv('AzureSecondPhaseSetPieces.csv')
+# replace with empty value, all rows in game_id that start by 'g'
 azureSecondPhaseSetPieces['game_id'].replace(regex=True, inplace=True, to_replace=r'g', value=r'')
+# replace with empty value, all rows in game_id that start by 'f'
 azureSecondPhaseSetPieces['game_id'].replace(regex=True, inplace=True, to_replace=r'f', value=r'')
+# list of unique game ids
 azureSecondPhaseSetPieces_game_id = list(azureSecondPhaseSetPieces['game_id'].unique())
 
 del azureSecondPhaseSetPieces
@@ -42,19 +54,23 @@ for item in allOptaCoreStats_game_id:
 [item for item in allOptaCoreStats_game_id if item not in azureOptaCoreStats_game_id]
 '''
 
+#creating a file with the game ids missing in Opta Core Stats
 text_file = open("missingInAzure.txt", "w")
 n = text_file.write(str([item for item in allOptaCoreStats_game_id if item not in azureOptaCoreStats_game_id]))
 text_file.close()
 
+# getting the game ids missing in azureOptaCoreStats compared to allOptaCoreStats
 CoreStatsMissing = allOptaCoreStats[allOptaCoreStats['Game ID'].isin(
     [item for item in allOptaCoreStats_game_id if item not in azureOptaCoreStats_game_id])]
 CoreStatsMissingGroupByCount = CoreStatsMissing[['Competition Name', 'Game ID', 'Season Name']].drop_duplicates() \
     .groupby(['Competition Name', 'Season Name']).agg({"Game ID": "sum"})
 
+#creating a file with the game ids missing in Set Pieces
 text_file = open("missingInSetPieces.txt", "w")
 n = text_file.write(str([item for item in azureOptaCoreStats_game_id if item not in azureSecondPhaseSetPieces_game_id]))
 text_file.close()
 
+# getting the game ids missing in azureSecondPhaseSetPieces compared to azureOptaCoreStats
 missingInSecondPhaseSetPieces = azureOptaCoreStats[azureOptaCoreStats['Game ID'].isin(
     [item for item in azureOptaCoreStats_game_id if item not in azureSecondPhaseSetPieces_game_id])]
 missingInSecondPhaseSetPiecesGroupByCount = missingInSecondPhaseSetPieces[
